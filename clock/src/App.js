bd933496322a4e55c1eca5512ec2f46d0eb185eb
tevee <brydiginte69@gmail.com>
2024-10-04 09:33:45 +0200

    added useEffect for effiency
    Added useEffect hook in order to clear the interval whenever the component unmounts

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Clock from './components/Clock.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [newDate, setNewDate] = useState(new Date());
 
-  setInterval(() => {
-    setNewDate(new Date());
-  }, 1000);
+  // Körs bara en gång när komponenten monteras med en tom array som dependency
+  // När komponenten avmonteras försvinner intervallet
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNewDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <div className="App">
